11f92b3934c875a95adc51186b87de8887b2224b
Michael <michaelfaleiro@gmail.com>
2023-06-07 01:06:57 -0300

    campo busca

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, url_for, jsonify
+from flask import render_template, url_for, jsonify, request
 import gspread
 
 gc = gspread.service_account(filename='service_account.json')
@@ -12,11 +12,23 @@ def index():
     return render_template('base.html')
 
 
-@app.route('/listar')
+@app.route('/listar', methods=['GET', 'POST'])
 def listar_envios():
     worksheet = sh.worksheet("Teste")
     data = {}
-    data = worksheet.get_all_records()
+
+    if request.method == 'POST':
+        search = request.form.get('search')
+        values = worksheet.findall(search)
+        print(values)
+        print(worksheet.find(search))
+        list = []
+        for rows in values:
+            list.append(worksheet.row_values(rows.row))
+
+            data = list
+    else:
+        data = worksheet.get_all_records()
     return render_template('listar_envios.html', values=data)
 
 
